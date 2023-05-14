8f5f9102ff1e5feb3a78a757fe21064c13b11713
Yemi Kelani <91276489+yemi-kelani@users.noreply.github.com>
2023-05-14 18:51:54 -0500

    Base Language Model created

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,10 +1,24 @@
 import json
 import pandas as pd
+import regex as re
 from keyword_extractor import KeywordExtractor
 
 DEV_PATH   = "../data/dev_rand_split.jsonl"
 TEST_PATH  = "../data/test_rand_split_no_answers.jsonl"
 TRAIN_PATH = "../data/train_rand_split.jsonl"
+
+# convert ConceptNet relation to broken natural language
+def relation2sentence(node):
+    subject1 = node["start"]
+    subject2 = node["end"]
+    relation = re.split('(?<=.)(?=[A-Z])', " " + node["relationship"] + " ")
+    if relation[0] != "is" or relation[0] != "Is":
+        relation.insert(0, "is")
+    
+    text = subject1 + " " + " ".join(relation) + subject2 + "."
+    return text.lower()
+
+print(relation2sentence({'start': 'altissimo', 'end': 'high', 'relationship': 'SimilarTo', 'weight': 2.0}))
 
 def process_raw_data( path: str, split: str, extractor_type: str = "keybert", limit:int = 5):
     # create dataframe from json lines
